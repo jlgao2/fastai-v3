365dfbc2fcd51e5cc50e9111dc53bb861ac758e9
George <georgegao888@gmail.com>
2020-05-05 20:31:23 +1000

    call pdr only once + odds > double

--- app/server.py
+++ app/server.py
@@ -60,8 +60,9 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    odds = learn.predict(img)[2][learn.predict(img)[1]]
+    pdr = learn.predict(img)
+    prediction = pdr[0]
+    odds = pdr[2][pdr[1]].double()
     return JSONResponse({'result': str(prediction), 'odds': str(odds)})
 
 
