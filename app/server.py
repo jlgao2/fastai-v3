5cf098a49f28529f66a7dfb033a493f7ba515bf5
George <georgegao888@gmail.com>
2020-05-05 20:13:30 +1000

    see if this works

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    odds = learn.predict(img)[3]
+    odds = learn.predict(img)[2][learn.predict(img)[1]]
     return JSONResponse({'result': str(prediction), 'odds': str(odds)})
 
 
